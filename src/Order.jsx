b16a0467453e9aa22684e9b04506ca8e0903c94e
sonu <sonugupta6045@gmail.com>
2024-04-22 22:28:31 +0530

    new change hass beeen donw

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 import Papa from "papaparse"; // Import Papaparse for CSV parsing
 import Table3 from "./Itemview.jsx";
 
@@ -53,28 +53,28 @@ function Inventory() {
 
     try {
       // Add item to Firestore collection
-      const docRef = await addDoc(collection(firestore, 'items'), newUser);
-      console.log('Document written with ID: ', docRef.id);
+      const docRef = await addDoc(collection(firestore, "items"), newUser);
+      console.log("Document written with ID: ", docRef.id);
 
       setUsers([...users, newUser]);
       setTotal(total + sum);
-      setName('');
+      setName("");
       setQty(0);
       setPrice(0);
     } catch (error) {
-      console.error('Error adding document: ', error);
+      console.error("Error adding document: ", error);
     }
   };
 
   const handleCsvUpload = async (event) => {
     const file = event.target.files[0];
     const storageRef = ref(storage, `uploads/${file.name}`);
-    
+
     try {
       // Upload the file to Firebase Storage
       await uploadBytes(storageRef, file);
-      console.log('File uploaded successfully.');
-      
+      console.log("File uploaded successfully.");
+
       // Parse the CSV file and update the inventory
       Papa.parse(file, {
         header: true,
@@ -82,11 +82,11 @@ function Inventory() {
           setInventory(result.data);
         },
         error: (error) => {
-          console.error('Error parsing CSV:', error);
-        }
+          console.error("Error parsing CSV:", error);
+        },
       });
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error("Error uploading file:", error);
     }
   };
 
@@ -95,103 +95,105 @@ function Inventory() {
   };
 
   return (
-    <div className="container-fluid bg-2 text-center">
-      <h1 align="center">Update the Stock</h1>
-      <br />
-      <div className="row">
-        <div className="col-sm-8">
-        <h3 className="title2"> Add Products </h3>
+    <div className="product">
+      <div className="container-fluid bg-2 text-center">
+        <h1 align="center">Update the Stock</h1>
+        <br />
+        <div className="row">
+          <div className="col-sm-8">
+            <h3 className="title2"> Add Products </h3>
             <br />
-          <table className="table table-bordered">
-            <thead>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Product name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th></th>
+                </tr>
+              </thead>
               <tr>
-                <th>Product name</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
-                <th></th>
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Item Name"
+                    value={name}
+                    onChange={(event) => {
+                      setName(event.target.value);
+                    }}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Price"
+                    value={price}
+                    onChange={handlePriceChange}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Qty"
+                    value={qty}
+                    onChange={handleQuantityChange}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={calculateTotal()}
+                    className="form-control"
+                    placeholder="Enter Total"
+                    id="total_cost"
+                    name="total_cost"
+                    disabled
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-success"
+                    type="button"
+                    onClick={handleAddItem}
+                  >
+                    Add
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tr>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Item Name"
-                  value={name}
-                  onChange={(event) => {
-                    setName(event.target.value);
-                  }}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Price"
-                  value={price}
-                  onChange={handlePriceChange}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Qty"
-                  value={qty}
-                  onChange={handleQuantityChange}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={calculateTotal()}
-                  className="form-control"
-                  placeholder="Enter Total"
-                  id="total_cost"
-                  name="total_cost"
-                  disabled
-                />
-              </td>
-              <td>
-                <button
-                  className="btn btn-success"
-                  type="button"
-                  onClick={handleAddItem}
-                >
-                  Add
-                </button>
-              </td>
-            </tr>
-          </table>
-          <h3 align="left"> Upload CSV </h3>
-          <input type="file" onChange={handleCsvUpload} accept=".csv" />
-          <h3 align="left"> History </h3>
-          {/* Display the singular entry update form */}
-          <Table3 items={[...users, ...inventory]} />
-        </div>
+            </table>
+            <h3 align="left"> Upload CSV </h3>
+            <input type="file" onChange={handleCsvUpload} accept=".csv" />
+            <h3 align="left"> History </h3>
+            {/* Display the singular entry update form */}
+            <Table3 items={[...users, ...inventory]} />
+          </div>
 
-        <div className="col-sm-4">
-          <div className="form-group" align="left">
-            <h3>Total</h3>
+          <div className="col-sm-4">
+            <div className="form-group" align="left">
+              <h3>Total</h3>
 
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Total"
-              required
-              disabled
-              value={total}
-            />
-            <br />
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={refreshPage}
-            >
-              {" "}
-              <span>Complete</span>{" "}
-            </button>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Total"
+                required
+                disabled
+                value={total}
+              />
+              <br />
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={refreshPage}
+              >
+                {" "}
+                <span>Complete</span>{" "}
+              </button>
+            </div>
           </div>
         </div>
       </div>
